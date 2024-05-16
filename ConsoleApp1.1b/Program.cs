37b41df11a0d8488be29c081017277ee250f3ddc
joshu <joshu@REEDPC>
2024-05-16 14:39:39 -0500

    Add project files.

---
+++ ConsoleApp1.1b/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+int numOfChildren = 2;
+int numOfPets = 4;
+double hourlyRate;
+hourlyRate = 53.25;
+double annualSalary;
+annualSalary = 100000;
+decimal grainsOfSandOnBeach;
+
+    int numChildrenAndPets = numOfChildren + numOfPets;
+Console.WriteLine(numChildrenAndPets);
+
+double hoursworked = annualSalary / (Convert.ToDouble(hourlyRate));
+Console.WriteLine(hoursworked);
